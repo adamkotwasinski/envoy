10a0bfe6fd83a764ef0ae0bc73b4a68f0fc3091b
Adam Kotwasinski <adam.kotwasinski@gmail.com>
2020-11-06 22:42:15 -0800

    FORMAT BUILD FILES

--- format.sh
+++ format.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ci/run_envoy_docker.sh 'ci/check_and_fix_format.sh'
-#export BUILDIFIER_BIN=/usr/local/bin/buildifier
-#export CLANG_FORMAT=/usr/local/bin/clang-format
-#tools/check_format.py fix
-#tools/proto_format.sh fix
+export BUILDOZER_BIN=/Users/adam.kotwasinski/go/bin/buildozer
+export BUILDIFIER_BIN=/usr/local/bin/buildifier
+export CLANG_FORMAT=/usr/local/bin/clang-format
+tools/code_format/check_format.py fix
+tools/code_format/format_python_tools.sh fix
+tools/code_format/check_spelling_pedantic.py fix
