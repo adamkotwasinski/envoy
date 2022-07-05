dbee8da320f2d599f6e6b3aee998094a50124d4e
Adam Kotwasinski <adam.kotwasinski@gmail.com>
2022-07-05 12:38:25 -0700

    kafka: compute partitions from configuration + ignore offset spec present in request

--- contrib/kafka/filters/network/source/mesh/command_handlers/fetch.cc
+++ contrib/kafka/filters/network/source/mesh/command_handlers/fetch.cc
@@ -24,23 +24,22 @@ AbstractResponseSharedPtr FetchRequestHolder::computeAnswer() const {
   std::ostringstream debug;
 
   const std::vector<FetchTopic> topics = request_->data_.topics_;
-  std::vector<FetchSpec> fetches_requested;
+  FetchSpec fetches_requested;
   for (const auto topic : topics) {
     const std::string topic_name = topic.topic_;
     const std::vector<FetchPartition> partitions = topic.partitions_;
     for (const auto partition : partitions) {
       const int32_t partition_id = partition.partition_;
-      const int64_t fetch_offset = partition.fetch_offset_;
-      ENVOY_LOG(trace, "Fetch for {}-{} / offset = {}", topic_name, partition_id, fetch_offset);
-      const FetchSpec spec = std::make_tuple(topic.topic_, partition.partition_, partition.fetch_offset_);
-      fetches_requested.push_back(spec);
-      debug << topic_name << "-" << partition_id << "=" << fetch_offset << ", ";
+      ENVOY_LOG(trace, "Fetch for {}-{}", topic_name, partition_id);
+      fetches_requested[topic_name].push_back(partition_id); // yay
+      
+      debug << topic_name << "-" << partition_id << ", "; // aaa
     }
   }
 
   ENVOY_LOG(info, "fetch: {} -> {}", fetches_requested.size(), debug.str());
 
-  consumer_manager_.requestFetches(fetches_requested);
+  consumer_manager_.processFetches(fetches_requested);
 
   const auto& header = request_->request_header_;
   const ResponseMetadata metadata = {header.api_key_, header.api_version_, header.correlation_id_};
