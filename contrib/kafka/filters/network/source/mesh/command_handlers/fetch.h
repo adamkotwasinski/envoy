38b44c0643fd549e103b4a028a8cd7e9a35b1d02
Adam Kotwasinski <adam.kotwasinski@gmail.com>
2022-07-07 16:23:09 -0700

    kafka: pass kafka messages to fetch request object

--- contrib/kafka/filters/network/source/mesh/command_handlers/fetch.h
+++ contrib/kafka/filters/network/source/mesh/command_handlers/fetch.h
@@ -4,6 +4,9 @@
 #include "contrib/kafka/filters/network/source/mesh/abstract_command.h"
 #include "contrib/kafka/filters/network/source/mesh/shared_consumer_manager.h"
 #include "contrib/kafka/filters/network/source/mesh/upstream_kafka_consumer.h"
+#include "contrib/kafka/filters/network/source/mesh/command_handlers/fetch_record.h"
+
+#include "absl/synchronization/mutex.h"
 
 namespace Envoy {
 namespace Extensions {
@@ -24,14 +27,25 @@ public:
 
   AbstractResponseSharedPtr computeAnswer() const override;
 
+  // Whether the given fetch request should be sent downstream.
+  // Typical cases are:
+  // - it has enough records (meeting request's minimal requirements),
+  // - enough time has passed.
+  bool isEligibleForSendingDownstream() const;
+
   // RecordCb
-  void accept() override;
+  bool receive(RdKafkaMessagePtr message) override;
 
 private:
   // Provides access to upstream-pointing consumers.
   SharedConsumerManager& consumer_manager_;
   // Original request.
   const std::shared_ptr<Request<FetchRequest>> request_;
+  // The messages to send downstream.
+  mutable absl::Mutex messages_mutex_;
+  std::vector<RdKafkaMessagePtr> messages_ ABSL_GUARDED_BY(messages_mutex_);
+  // Translates librdkafka objects into bytes to be sent downstream.
+  const FetchResponsePayloadProcessor processor_;
 };
 
 } // namespace Mesh
