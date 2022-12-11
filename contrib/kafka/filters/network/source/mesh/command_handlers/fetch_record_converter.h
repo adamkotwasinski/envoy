3ad32033c7857d7ec8f0eb5a4d9ca5b3981029ef
Adam Kotwasinski <adam.kotwasinski@gmail.com>
2022-12-10 22:25:47 -0800

    kafka: fetch record converter
    Signed-off-by: Adam Kotwasinski <adam.kotwasinski@gmail.com>

---
+++ contrib/kafka/filters/network/source/mesh/command_handlers/fetch_record_converter.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "source/common/common/logger.h"
+
+#include "contrib/kafka/filters/network/source/external/responses.h"
+#include "contrib/kafka/filters/network/source/kafka_types.h"
+#include "contrib/kafka/filters/network/source/mesh/inbound_record.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace NetworkFilters {
+namespace Kafka {
+namespace Mesh {
+
+class FetchResponsePayloadProcessor : private Logger::Loggable<Logger::Id::kafka> {
+public:
+  std::vector<FetchableTopicResponse>
+  transform(const std::map<KafkaPartition, std::vector<InboundRecordSharedPtr>>& arg) const;
+
+private:
+  void print(const Bytes& arg, const std::string ctx = "arg") const;
+
+  void append(Bytes& out, const InboundRecordSharedPtr& ptr) const;
+};
+
+} // namespace Mesh
+} // namespace Kafka
+} // namespace NetworkFilters
+} // namespace Extensions
+} // namespace Envoy
